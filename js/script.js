372f9189f0b29dff43ca522400b5bbba930391fa
TomaszPosluszny <tomkposluszny@gmail.com>
2022-10-17 18:30:11 +0200

    cd

--- js/script.js
+++ js/script.js
@@ -4,4 +4,15 @@ const li = document.querySelectorAll('li');
 const searchEngine = e => {
     const text = e.target.value.toLowerCase();
 
-}
+
+
+li.forEach(el => {
+    if (el.textContent.toLowerCase().indexOf(text) !== -1) {
+        el.style.display = 'block'
+    } else {
+        el.style.display = 'none'
+    }
+})
+};
+
+search.addEventListener('keyup', searchEngine);
